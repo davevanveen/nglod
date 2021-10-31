f179b1c170df80b7b1c9519631d24c01810e37ea
Dave Van Veen <davemvanveen@gmail.com>
2021-10-31 12:10:23 -0700

    updated script

--- sdf-net/train.sh
+++ sdf-net/train.sh
@@ -1,9 +1,9 @@
-shape=bunny
+shape=lucy
 
 python3.8 app/main.py \
     --net OctreeSDF \
     --num-lods 4 \
     --dataset-path /home/vanveen/nglod/data/$shape.obj \
     --epoch 250 \
-    --gpu 7 \
+    --gpu 5 \
     --exp-name $shape
